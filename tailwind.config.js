fa568af5aa221dfc79f004c5d8fe4a08b4074054
Krish Singh <singhkrish.np@gmail.com>
2025-11-18 18:56:27 +0530

    Build Tailwind locally: replace CDN with compiled CSS; add tailwind.config + scripts; update Vercel build

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './public/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
